42791374d4e973e89911e71900e645dff4b5b970
Johnny <joaogardenberg@gmail.com>
2025-02-11 12:57:51 +0100

    10-textures - Finished

--- 10-textures/src/script.js
+++ 10-textures/src/script.js
@@ -1,6 +1,45 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+// Textures
+const loadingManager = new THREE.LoadingManager()
+
+loadingManager.onStart = () => console.log('Started')
+loadingManager.onProgress = (...a) => console.log('Progress', ...a)
+loadingManager.onLoad = () => console.log('Loaded')
+loadingManager.onError = (...a) => console.log('Error', ...a)
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+// const mapTexture = textureLoader.load('/textures/door/color.jpg')
+// const mapTexture = textureLoader.load('/textures/checkerboard-1024x1024.png')
+// const mapTexture = textureLoader.load('/textures/checkerboard-8x8.png')
+const mapTexture = textureLoader.load('/textures/minecraft.png')
+mapTexture.colorSpace = THREE.SRGBColorSpace
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load(
+  '/textures/door/ambientOcclusion.jpg'
+)
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+// mapTexture.repeat.x = 2
+// mapTexture.repeat.y = 3
+// mapTexture.wrapS = THREE.RepeatWrapping
+// mapTexture.wrapT = THREE.RepeatWrapping
+
+// mapTexture.offset.x = 0.5
+// mapTexture.offset.y = 0.5
+
+// mapTexture.rotation = Math.PI / 4
+// mapTexture.center.x = 0.5
+// mapTexture.center.y = 0.5
+
+mapTexture.generateMipmaps = false
+mapTexture.minFilter = THREE.NearestFilter
+mapTexture.magFilter = THREE.NearestFilter
+
 /**
  * Base
  */
@@ -14,7 +53,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ map: mapTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -22,30 +61,34 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
 camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 1
@@ -59,7 +102,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -69,18 +112,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
