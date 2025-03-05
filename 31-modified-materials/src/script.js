09430d1273b5470e7ef7d29bd6bede88c835fa20
Johnny <joaogardenberg@gmail.com>
2025-03-05 01:39:31 +0100

    31-modified-materials - Finished

--- 31-modified-materials/src/script.js
+++ 31-modified-materials/src/script.js
@@ -69,6 +69,78 @@ const material = new THREE.MeshStandardMaterial({
   normalMap: normalTexture
 })
 
+const depthMaterial = new THREE.MeshDepthMaterial({
+  depthPacking: THREE.RGBADepthPacking
+})
+
+const customUniforms = {
+  uTime: { value: 0 }
+}
+
+material.onBeforeCompile = (shader) => {
+  shader.uniforms.uTime = customUniforms.uTime
+
+  shader.vertexShader = shader.vertexShader
+    .replace(
+      '#include <common>',
+      `
+        #include <common>
+
+        uniform float uTime;
+
+        mat2 get2dRotationMatrix(float _angle) {
+          return mat2(cos(_angle), -sin(_angle), sin(_angle),  cos(_angle));
+        }
+      `
+    )
+    .replace(
+      '#include <beginnormal_vertex>',
+      `
+        #include <beginnormal_vertex>
+
+        float angle = (position.y + uTime) * 0.5;
+        mat2 rotationMatrix = get2dRotationMatrix(angle);
+        objectNormal.xz *= rotationMatrix;
+      `
+    )
+    .replace(
+      '#include <begin_vertex>',
+      `
+        #include <begin_vertex>
+
+        transformed.xz *= rotationMatrix;
+      `
+    )
+}
+
+depthMaterial.onBeforeCompile = (shader) => {
+  shader.uniforms.uTime = customUniforms.uTime
+
+  shader.vertexShader = shader.vertexShader
+    .replace(
+      '#include <common>',
+      `
+        #include <common>
+
+        uniform float uTime;
+
+        mat2 get2dRotationMatrix(float _angle) {
+          return mat2(cos(_angle), -sin(_angle), sin(_angle),  cos(_angle));
+        }
+      `
+    )
+    .replace(
+      '#include <begin_vertex>',
+      `
+        #include <begin_vertex>
+
+        float angle = (position.y + uTime) * 0.5;
+        mat2 rotationMatrix = get2dRotationMatrix(angle);
+        transformed.xz *= rotationMatrix;
+      `
+    )
+}
+
 /**
  * Models
  */
@@ -77,11 +149,24 @@ gltfLoader.load('/models/LeePerrySmith/LeePerrySmith.glb', (gltf) => {
   const mesh = gltf.scene.children[0]
   mesh.rotation.y = Math.PI * 0.5
   mesh.material = material
+  mesh.customDepthMaterial = depthMaterial
   scene.add(mesh)
 
   // Update materials
   updateAllMaterials()
 })
+
+/**
+ * Plane
+ */
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(15, 15, 15),
+  new THREE.MeshStandardMaterial()
+)
+plane.rotation.y = Math.PI
+plane.position.y = -5
+plane.position.z = 5
+scene.add(plane)
 
 /**
  * Lights
@@ -154,6 +239,9 @@ const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+
+  // Update material
+  customUniforms.uTime.value = elapsedTime
 
   // Update controls
   controls.update()
